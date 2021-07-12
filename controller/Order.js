d254aa54d9163f5b20e77fba11902f7b718d947b
reshmaparhar <parharra17.it@coep.ac.in>
2021-07-12 17:43:28 +0530

    added getproducts

--- controller/Order.js
+++ controller/Order.js
@@ -5,48 +5,77 @@ const { query } = require('express');
 const Order = require('../databases/mongo/models/Order');
 
 
-const PlaceOrder =async(req,res)=>{
-    try{
-        
+const PlaceOrder = async (req, res) => {
+    try {
+
         const user = await User.findById(req.body.userId);
-       
-        if(user){
+
+        if (user) {
             const product = await Product.findById(req.body.productId)
-            
-            if (product){
-                
-                if(product.availableQuantity >= Number(req.body.quantity)){
-                            const newquantity = product.availableQuantity - req.body.quantity
-                            await Product.findByIdAndUpdate(req.body.productId, {"availableQuantity":newquantity},{new: true})
-                            const order = new Order({
-                                "orderCreatedBy":req.body.userId,
-                                "productId":req.body.productId,
-                                "quanity":req.body.quantity,
-                                "totalCost":(req.body.quantity * product.price)
-                            })
-                           
-                            
-                            await order.save()
-                            return res.json(responseFunction(true,`Order Placed Successfully`,order))
-                        }
-                        else{
-                            return res.json(responseFunction(false,`Sorry !! Available quantity of product is less than required quantity`,null))
-                        }
+
+            if (product) {
+
+                if (product.availableQuantity >= Number(req.body.quantity)) {
+                    const newquantity = product.availableQuantity - req.body.quantity
+                    await Product.findByIdAndUpdate(req.body.productId, { "availableQuantity": newquantity }, { new: true })
+                    const order = new Order({
+                        "orderCreatedBy": req.body.userId,
+                        "productId": req.body.productId,
+                        "quanity": req.body.quantity,
+                        "totalCost": (req.body.quantity * product.price)
+                    })
+
+
+                    await order.save()
+                    return res.json(responseFunction(true, `Order Placed Successfully`, order))
+                }
+                else {
+                    return res.json(responseFunction(false, `Sorry !! Available quantity of product is less than required quantity`, null))
+                }
             }
-            else{
-                return res.json(responseFunction(false,`Sorry !! Product with this id does not exists in database`,null))
+            else {
+                return res.json(responseFunction(false, `Sorry !! Product with this id does not exists in database`, null))
             }
         }
-        else{
-            return res.json(responseFunction(false,`Sorry !! user with this id does not exists in database`,null))
+        else {
+            return res.json(responseFunction(false, `Sorry !! user with this id does not exists in database`, null))
         }
-            
-          
-       
-          
+
+
+
+
     }
-    catch(error){
-        res.json(responseFunction(false,error.message,null))
+    catch (error) {
+        res.json(responseFunction(false, error.message, null))
     }
-};
-module.exports = PlaceOrder;
+}
+const getOrder = async (req, res) => {
+    try {
+        const orderDetails = [];
+        var limit = parseInt(req.query.limit);
+        var skip = (parseInt(req.query.page) - 1) * parseInt(limit);
+        console.log(limit);
+        console.log(skip);
+        const user = await User.findById(req.params.userId);
+        if (user) {
+            const order_detail = {};
+            const orders = await Order.find().skip(skip).limit(limit);
+            console.log(orders)
+            for (var i in orders) {
+                order_detail.order = orders[i];
+                product = await Product.findById(orders[i].productId);
+                order_detail.productName = product.name;
+                orderDetails.push(order_detail);
+            }
+            return res.json(responseFunction(true, `Details fetched successfully`, orderDetails));
+        }
+        else {
+            return res.json(responseFunction(false, `Sorry !! user with this id does not exists in database`, null))
+        }
+    }
+    catch (err) {
+        res.json(responseFunction(false, error.message, null))
+    }
+}
+
+module.exports = { PlaceOrder, getOrder };
--- routers/order_routes.js
+++ routers/order_routes.js
@@ -3,6 +3,7 @@ const app = express();
 const router = express.Router();
 const validation = require('../middlewares/validator/validation');
 const schemas = require('../middlewares/validator/OrderSchema');
-const PlaceOrder = require('../controller/o1')
+const {PlaceOrder,getOrder} = require('../controller/Order')
 router.route('/').post(validation(schemas.Place_Order),PlaceOrder)
+router.route('/:userId').get(getOrder);
 module.exports = router;
